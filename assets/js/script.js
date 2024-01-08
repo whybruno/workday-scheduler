c1870b1e323e0744cbc1946d2a7fc449d303e5b0
whybruno.dev@gmail.com <whybruno.dev@gmail.com>
2024-01-08 20:00:31 +0000

    Add updateHour() to script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,4 +9,33 @@ $(document).ready(function() {
   // calls the displayTime() every second
   setInterval(displayTime, 1000);
 
+  // updateHour() : update current time status
+  function updateHour() {
+    // obtaining the current hour using the dayjs().hour() method
+    let currentHour = dayjs().hour();
+    // selects all elements with the class .time-block
+    let timeBlocks = $('.time-block');
+    
+    // timeBlocks() : iterates through the selected time blocks
+    timeBlocks.each(function() {
+      // extracts the hour from the element's ID
+      let blockHour = parseInt($(this).attr('id'));
+      // applies appropriate classes to the element
+      if (currentHour > blockHour) {
+        $(this).addClass('past');
+      } else if (currentHour === blockHour) {
+        $(this).removeClass('past');
+        $(this).addClass('present');
+      } else {
+        $(this).removeClass('past');
+        $(this).removeClass('present');
+        $(this).addClass('future');
+      };
+    });
+  };
+  
+  // calls the updateHour() every minute
+  updateHour();
+  setInterval(updateHour, 60000);
+
 });
